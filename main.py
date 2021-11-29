55d26324fa7e0129109c63dc68cc51d96b912356
Deepak Sinha <sinhadeepak1115study@gmail.com>
2021-11-29 15:16:35 +0530

    completed navigation bar function for nav bar

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
