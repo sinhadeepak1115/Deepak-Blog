12e636b1cc15d01a9f213f127173cb9ffff57e26
Deepak Sinha <sinhadeepak1115study@gmail.com>
2021-12-01 00:32:38 +0530

    use flask and contact functional.

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
-post = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
-@app.route('/about.html')
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route('/contact.html')
+@app.route("/contact.html", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
-
-@app.route('/index.html')
-def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template("contact.html",msg_sent=True)
+    return render_template("contact.html",msg_sent=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
